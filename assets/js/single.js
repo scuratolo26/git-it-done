23a1962a41bc992c7e32d71eaa46c33525c7f06b
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-07-14 15:19:52 -0400

    show issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var displayWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    // create link element
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var getRepoIssues = function (repo) {
     var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +22,11 @@ var getRepoIssues = function (repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    console.log("repo has more than 30 issues");
+                }
             });
         }
         else {
